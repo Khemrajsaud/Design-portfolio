45fffd13b265a5bff8015434f917209b9f78df78
Khemraj Saud <khemrajsaud56@gmail.com>
2024-12-23 13:05:10 +0545

    edit file

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,17 +4,17 @@ import myProfile from "../Components/Image/myProfile.png"
 const Home = () => {
   return (
     <div className='bg-red-100'>
-        <div className='lg:flex items-center justify-between   md:grid-flow-col gap-8'>
-            <div className=' lg:w-1/2 lg:my-28 p-9 '>
-                <h3 className='text-2xl text-green-500'>Hi I'm Khemraj Saud</h3><br />
-                <h1 className='text-4xl font-bold   md:text-2xl text-yellow-500 sm:text-balance'> Mern Stack Developer</h1><br /><br />
+        <div className='lg:flex items-center justify-between  md:grid-flow-col gap-8'>
+            <div className=' lg:w-1/2 lg:my-28 p-6 '>
+                <h3 className=' text-2xl font-bold'>Hi I'm Khemraj Saud</h3><br />
+                <h1 className='text-4xl font-bold   md:text-2xl text-yellow-500 '> Mern Stack Developer</h1><br /><br />
                 <p className=''>I am a dedicated and passionate MERN-Stack developer with a strong foundation in modern web development technologies.My goal is to leverage my skills and knowledge to contribute to innovative web applications that solve real-world problems.</p>
                 <br />
 
                 <button className='border-spacing-1 rounded-md p-2 px-3 border-2  border-blue-500 shadow-gray-300'>My Project</button>
             </div>
-            <div className='flex items-center justify-center lg:w-1/2'>
-               <img className='w-[300px] rounded-full  '  src={myProfile} alt="" />
+            <div className='flex items-center justify-center lg:w-1/2 pb-8'>
+               <img className='lg:w-[300px] w-[250px] rounded-full  '  src={myProfile} alt="" />
             </div>
         </div>
       
--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -1,78 +1,97 @@
 import React, { useState } from "react";
 
 import project1 from "../Components/Image/project1.png";
-import project2 from  "../Components/Image/project2.png";
+import project2 from "../Components/Image/project2.png";
 import project3 from "../Components/Image/project3.png";
 import { FaGithub } from "react-icons/fa";
-const projects =[
+const projects = [
   {
     imgs: project1,
     title: "Project #1",
     description: "UI for fronted development using React",
     links: {
       site: "https://www.netflix.com/np/",
-      github: "https://github.com/Khemrajsaud"
-    }
-  }
-  , {
+      github: "https://github.com/Khemrajsaud",
+    },
+  },
+  {
     imgs: project2,
     title: "Project #2",
-    description: "UI for fronted development using React",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit hic tenetur vero atque aliquam enim odit, aut accusantium! Voluptate accusantium mollitia laudantium blanditiis doloribus numquam sit fugiat inventore dolorem rem",
     links: {
       site: "#",
-      github: "#"
-    }
-  }
-  , {
+      github: "#",
+    },
+  },
+  {
     imgs: project3,
     title: "Project #3",
     description: "UI for fronted development using React",
     links: {
       site: "#",
-      github: "#"
-    }
-  }
-  
-  
-]
+      github: "#",
+    },
+  },
+];
 
-const ProjectCard = () =>{
+const ProjectCard = () => {
+  const [currentPfoject, setCurrentProject] = useState(0);
 
-const [currentPfoject, setCurrentProject] = useState(0)
-  
   return (
-    <div className=" flex-col md:flex-row bg-red-80   " id="portfolio">
-        <h1 className="text-3xl text-center font-bold underline decoration-4 decoration-pink-500  underline-offset-8
-        pt-8  ">Latest Project</h1>
+    <div className=" flex-col  bg-red-80   " id="portfolio">
+      <h1
+        className="text-3xl text-center font-bold underline decoration-4 decoration-pink-500  underline-offset-8
+        pt-8  "
+      >
+        Latest Project
+      </h1>
       <div className=" py-10">
-      <div className="p-6 lg:ml-[350px] border-2 lg:w-1/2  bg-gray-700">
-      
-        <div className=" rounded-md ">
-          <img src={projects[currentPfoject].imgs}  alt={projects[currentPfoject].title} className="w-full h-full object-cover rounded-lg mb-4" />
+        <div className="p-6 lg:ml-[350px] border-2 lg:w-1/2 rounded-md ">
+          <div className=" rounded-md bg-gray-100  shadow-lg ">
+            <img
+              src={projects[currentPfoject].imgs}
+              alt={projects[currentPfoject].title}
+              className="w-full h-full object-cover rounded-lg mb-4"
+            />
+          </div>
+          <p className="text-gray-800 my-4">
+            {projects[currentPfoject].description}
+          </p>
+          <div className="flex space-x-4">
+            <a
+              href={projects[currentPfoject].links.site}
+              className="px-4 py-2 bg-slate-500 text-gray-200 rounded-lg hover:bg-slate-700 transition duration-300"
+            >
+              view site
+            </a>
+            <a
+              className="px-5 bg-gray-800 text-gray-200 text-2xl rounded-lg hover:bg-gray-600 transition duration-200"
+              href={projects[currentPfoject].links.github}
+            >
+              <FaGithub />
+            </a>
+          </div>
         </div>
-        <p className="text-gray-200 my-4">
-          {projects[currentPfoject].description}
 
-        </p>
-        <div className="flex space-x-4">
-          <a href={projects[currentPfoject].links.site} className="px-4 py-2 bg-slate-500 text-gray-200 rounded-lg hover:bg-slate-700 transition duration-300">view site</a>
-          <a className="px-5 bg-gray-800 text-gray-200 text-2xl rounded-lg hover:bg-gray-600 transition duration-200" href={projects[currentPfoject].links.github}><FaGithub/></a>
+        <div>
+          <ul className=" flex flex-col w-[200px] gap-2 lg:ml-[350px] mt-3 ml-[80px]">
+            {projects.map((project, index) => (
+              <li
+                key={index}
+                onClick={() => setCurrentProject(index)}
+                className={`cursor-pointer text-gray-100 bg-slate-700 rounded-md p-2  text-center  hover:bg-slate-800 transition duration-300 ${
+                  currentPfoject === index ? "bg-slate-900" : ""
+                }`}
+              >
+                {project.title}
+              </li>
+            ))}
+          </ul>
+          <div></div>
         </div>
-
       </div>
-      <ul className="grid grid-cols-1 gap-2 lg:ml-[350px] sm:w-[100px]   ">
-        {projects.map((project,index) =>(
-          <li key={index} onClick={()=> setCurrentProject(index)} className={`cursor-pointer text-gray-300 bg-slate-700 rounded-lg p-2 text-center lg:max-w-[100px] hover:bg-slate-800 transition duration-300 ${currentPfoject === index? "bg-slate-900" : "" }`}>
-            {project.title}
-          </li>
-        ))}
-       
-      </ul>
-      </div>
-
     </div>
-    
-   
   );
 };
 
